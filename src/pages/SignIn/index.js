8ead34d1e7a4169bde7ff2ade6005841bd9a3657
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-15 17:45:06 -0300

    feat: create input and button components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-// import { Container } from './styles';
-
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+// import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input
+        icon="call"
+        placeholder="Isso é o placeholder"
+        style={{ marginTop: 30 }}
+      />
+      <Button>Entrar</Button>
     </Background>
   );
 }
