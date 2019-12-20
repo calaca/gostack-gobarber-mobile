8fe46c4e3b00815e5d1ac8161b79953db2e93257
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-20 19:49:57 -0300

    feat: load appointments

--- src/routes.js
+++ src/routes.js
@@ -54,6 +54,7 @@ export default (signedIn = false) =>
             Profile,
           },
           {
+            resetOnBlur: true,
             tabBarOptions: {
               keyboardHidesTabBar: true,
               activeTintColor: '#fff',
